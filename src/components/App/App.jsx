1b64b5938df533caa3071150a632268b8b9d73a4
Yevhen Lytvynchuk <evg.litik@gmail.com>
2022-09-01 18:04:04 +0300

    up contacts book

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -9,14 +9,27 @@ import { TitleApp, TitleContacts } from './App.styled';
 
 export class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     name: '',
   };
+
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    const nextContacts = this.state.contacts;
+    const prevContscts = prevState.contacts;
+
+    if (nextContacts !== prevContscts) {
+      localStorage.setItem('contacts', JSON.stringify(nextContacts));
+    }
+  }
 
   addContact = contact => {
     const availableCont = availableContact(this.state.contacts, contact);
